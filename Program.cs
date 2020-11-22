1143d43e8785416c6969c6380b9ea59f1cd745a7
Тимур Мухаметов <timurik2803@gmail.com>
2020-11-22 09:49:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет {name}");
+        }
+    }
+}
